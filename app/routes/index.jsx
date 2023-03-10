cc0157fced889627b3dde43824cead9634bc5618
Haris <hariskumar.eth@gmail.com>
2023-03-10 18:45:06 +0530

    Initial Commit

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,12 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="font-dmsans flex h-screen w-full font-bold text-xl justify-center items-center" style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+       <a href="https://metarover.vercel.app/metarover"
+        className="inline-flex items-center justify-center rounded font-bold bg-purple-600 text-xl px-4 py-2 hover:bg-purple-700 text-white  shadow-blackA7 outline-none  "
+        
+      >
+        Visit MetaRover
+      </a>
     </div>
   );
 }
